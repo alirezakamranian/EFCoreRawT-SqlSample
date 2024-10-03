2258bdd2fb68f6003c87510933081ceb080a0ca5
AlirezaKamranian <123934502+alirezakamranian@users.noreply.github.com>
2024-10-03 16:10:42 +0330

    Feat:Add user service

---
+++ RawT-SqlSample/RawT-SqlSample/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using RawT_SqlSample.DataAccess;
+using System.ComponentModel.DataAnnotations;
+
+namespace RawT_SqlSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController(DataContext context) : ControllerBase
+    {
+        private readonly DataContext _context =context;
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp([Required]string name, [Required]string phone)
+        {
+            object[] paramItems =
+            [
+                new SqlParameter("@name", name),
+                new SqlParameter("@phone", phone),
+                 new SqlParameter("@IsAuthor", false)
+            ];
+
+            await _context.Database.ExecuteSqlRawAsync("INSERT INTO Users(Name,PhoneNumber,IsAuthor) VALUES (@name,@phone,@IsAuthor)", paramItems);
+
+            return (Ok("huum?"));
+        }
+
+        [HttpPost("AddToAuthors")]
+        public async Task<IActionResult> AddToAuthors([Required]string userId)
+        {
+            object[] paramItems =
+            [
+                new SqlParameter("@IsAuthor", true),
+                new SqlParameter("@userId", userId)
+            ];
+            await _context.Database.ExecuteSqlRawAsync("UPDATE Users SET IsAuthor = @IsAuthor WHERE Id = @userId", paramItems);
+
+            return (Ok("huum?"));
+        }
+    }
+}
--- RawT-SqlSample/RawT-SqlSample/DataAccess/DataContext.cs
+++ RawT-SqlSample/RawT-SqlSample/DataAccess/DataContext.cs
@@ -3,13 +3,8 @@ using RawT_SqlSample.Models.Entities;
 
 namespace RawT_SqlSample.DataAccess
 {
-    public class DataContext : DbContext
+    public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
-        {
-
-        }
-
         public DbSet<User> Users { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Article> Articles { get; set; }
